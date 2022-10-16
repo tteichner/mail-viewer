fe26772266e89bcf9b89273d30f7ef27c102cf33
Tobias Teichner <webmaster@teichner.biz>
2022-10-16 21:26:30 +0200

    CHG: Upgrade to most recent package versions / ADD: Release script

---
+++ archive.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const archiver = require('archiver');
+const output = fs.createWriteStream('./RELEASE.zip');
+const archive = archiver('zip', {
+    gzip: true,
+    zlib: { level: 9 } // Sets the compression level.
+});
+
+archive.on('error', function(err) {
+    throw err;
+});
+
+// pipe archive data to the output file
+archive.pipe(output);
+
+// append files
+archive.file('./Readme.md', {name: 'README.md'});
+archive.directory('./dist/mail-viewer', 'server');
+
+//
+archive.finalize().then(() => {
+    console.log('Archive completed');
+});
