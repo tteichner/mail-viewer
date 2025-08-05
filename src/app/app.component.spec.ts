b039bee9edcf3fd78f8e41067931955cb6d96934
Tobias Teichner <webmaster@teichner.biz>
2025-08-05 11:02:43 +0200

    CHG: Adapt unit test suite to new implementations

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,46 +1,33 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterTestingModule } from '@angular/router/testing';
-import { BrowserModule } from '@angular/platform-browser';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule} from "@angular/router";
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        HttpClientModule,
-        RouterTestingModule,
-        RouterTestingModule.withRoutes([]),
-        BrowserModule,
-      ],
-      declarations: [
-        AppComponent
-      ],
-      providers: [
-        {
-          provide: 'APP_VERSION',
-          useValue: '1.0.0'
-        }
-      ]
-    }).compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [
+                RouterModule.forRoot([]),
+                BrowserModule,
+            ],
+            declarations: [
+                AppComponent
+            ],
+            providers: [
+                provideHttpClient(withInterceptorsFromDi()),
+                {
+                    provide: 'APP_VERSION',
+                    useValue: '1.0.0'
+                }
+            ]
+        }).compileComponents();
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'mail-viewer'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.version).toBe('1.0.0');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('span').textContent).toContain('Version');
-  });
+    it('should create app and render title', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement;
+        expect(compiled.querySelector('span').textContent).toContain('Version: 1.0.0');
+    });
 });
